2b21734df8480c6f4b04fcd8cfe05a9fbbf5fcbc
nksazonov <nsazonov@openware.com>
2025-07-02 21:08:46 +0300

    feat: update guests map

--- public/data/guestsMap.ts
+++ public/data/guestsMap.ts
@@ -5,14 +5,80 @@ export interface GuestsMap {
 };
 
 const guestsMap: GuestsMap = {
+  'vitaliy': {
+    text: 'Любий батьку Віталію'
+  },
+  'tetyana-lyudmyla': {
+    text: 'Люба мамо Тетяно та бабусе Людмило'
+  },
+  'inna': {
+    text: 'Люба Хрещена Інно'
+  },
+  'danylo-oleksandra': {
+    text: 'Друзі Данило та Олександро'
+  },
+  'artem': {
+    text: 'Друже Артеме'
+  },
+  'illya': {
+    text: 'Друже Іллє'
+  },
+  'eduard': {
+    text: 'Друже Едуарде'
+  },
+  'maksym': {
+    text: 'Шановний Максиме'
+  },
+  'danylo': {
+    text: 'Шановний Данило'
+  },
+  'anton': {
+    text: 'Шановний Антоне'
+  },
+  'oleksiy-nataliya': {
+    text: 'Любі батьки – Олексію та Наталіє'
+  },
   'kuzma': {
-    text: 'Рідний брате Кузьма'
+    text: 'Шановний брате Кузьма'
   },
-  'ilya': {
-    text: 'Дорогий Ілля'
+  'vadym': {
+    text: 'Дорогий Вадиме'
   },
-  'vlada': {
-    text: 'Люба Владиславо'
+  'valentyna': {
+    text: 'Люба бабусю – Валентино Омелянiвно'
+  },
+  'valentyna-mykolay': {
+    text: 'Любі бабусю та дідусю – Валентино та Миколо'
+  },
+  'larysa-roman': {
+    text: 'Дорогі Ларисо та Романе'
+  },
+  'anton-m': {
+    text: 'Дорогий брате Антоне'
+  },
+  'iryna-kevin': {
+    text: 'Дорогі Ірино та Кевіне'
+  },
+  'vladyslava': {
+    text: 'Люба сестро Владо'
+  },
+  'olena': {
+    text: 'Дорога Хрещена Олено'
+  },
+  'oleksandr': {
+    text: 'Дорогий Олександре'
+  },
+  'sofiya': {
+    text: 'Дорога подруго Соню'
+  },
+  'viktoriya-danylo': {
+    text: 'Дорогі Вікторіє та Даниле'
+  },
+  'svitlana-ruslan': {
+    text: 'Дорогі Світлано та Руслане'
+  },
+  'hennadiy': {
+    text: 'Дорогий Геннадію'
   }
 };
 
